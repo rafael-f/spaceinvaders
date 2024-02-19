5e7d39e01b05148f11aa263c32d004a8c453c37c
rforchetti <rforchetti@kokku.com.br>
2024-02-19 16:25:10 -0300

    OpenGL training shader basic.

--- src/SpaceInvaders/GameEngine.cpp
+++ src/SpaceInvaders/GameEngine.cpp
@@ -1,6 +1,48 @@
 #include "GameEngine.h"
 #include <Windows.h>
 #include <cstdlib>
+#include <string>
+#include <fstream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		// todo print error
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 GameEngine::GameEngine()
 {
@@ -25,8 +67,55 @@ GameEngine::GameEngine()
 	//m_ScreenManager = unique_ptr<ScreenManager>(new ScreenManager(Vector2i(m_Resolution.x, m_Resolution.y)));
 }
 
+std::string ReadFile(const std::string& fileName)
+{
+	std::ifstream file_stream(fileName);
+	if (file_stream.is_open())
+	{
+		std::string file_content;
+
+		std::string line;
+		while (std::getline(file_stream, line)) {
+			file_content += line + "\n";
+		}
+
+		file_stream.close();
+
+		return file_content;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 void GameEngine::run()
 {
+	const int size = 6;
+
+	// TODO remove opengl learning
+	float positions[size] = {
+		-.5f, -.5f,
+		.0f, 0.5f,
+		.5f, -.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	std::string vertexShader = ReadFile("VertexShader.hlsl");
+	std::string fragmentShader = ReadFile("PixelShader.hlsl");
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	while (!glfwWindowShouldClose(m_Window))
 	{
 	//	m_DT = m_Clock.restart();
@@ -35,6 +124,8 @@ void GameEngine::run()
 	//	update();
 		draw();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 }
@@ -54,11 +145,12 @@ void GameEngine::draw()
 	/* Render here */
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
+	/*glBegin(GL_TRIANGLES);
 	glVertex2f(-.5f, -.5f);
 	glVertex2f(.0f, 0.5f);
 	glVertex2f(.5f, -.5f);
-	glEnd();
+	glEnd();*/
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	/* Swap front and back buffers */
 	glfwSwapBuffers(m_Window);
