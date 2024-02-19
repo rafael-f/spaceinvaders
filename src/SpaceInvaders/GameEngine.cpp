eb07f27c74ddd391a143c07c053d2f27666350d2
rforchetti <rforchetti@kokku.com.br>
2024-02-19 18:02:53 -0300

    Added index buffers.

--- src/SpaceInvaders/GameEngine.cpp
+++ src/SpaceInvaders/GameEngine.cpp
@@ -94,19 +94,30 @@ void GameEngine::run()
 	const int size = 6;
 
 	// TODO remove opengl learning
-	float positions[size] = {
+	float positions[] = {
 		-.5f, -.5f,
-		.0f, 0.5f,
-		.5f, -.5f
+		.5f, -.5f,
+		.5f, .5f,
+		-.5f, .5f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	std::string vertexShader = ReadFile("VertexShader.hlsl");
 	std::string fragmentShader = ReadFile("PixelShader.hlsl");
@@ -145,12 +156,7 @@ void GameEngine::draw()
 	/* Render here */
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	/*glBegin(GL_TRIANGLES);
-	glVertex2f(-.5f, -.5f);
-	glVertex2f(.0f, 0.5f);
-	glVertex2f(.5f, -.5f);
-	glEnd();*/
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 	/* Swap front and back buffers */
 	glfwSwapBuffers(m_Window);
