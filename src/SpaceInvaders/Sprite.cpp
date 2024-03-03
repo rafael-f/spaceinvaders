cd3f8f6b31bd1b075aba64a8ebb6aafa8d89b0b1
rforchetti <rforchetti@kokku.com.br>
2024-03-03 11:20:17 -0300

    no message

--- src/SpaceInvaders/Sprite.cpp
+++ src/SpaceInvaders/Sprite.cpp
@@ -4,7 +4,16 @@
 
 Sprite::Sprite()
 {
-	
+	m_Shader.AddShader(GL_VERTEX_SHADER, "VertexShader.hlsl");
+	m_Shader.AddShader(GL_FRAGMENT_SHADER, "PixelShader.hlsl");
+	m_Shader.CreateShader();
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	m_IndexBuffer = IndexBuffer(indices, 6);
 }
 
 void Sprite::setTexture(const Texture& texture)
@@ -35,53 +44,42 @@ void Sprite::setPosition(Vector2f position)
 
 void Sprite::Draw(const Renderer& renderer)
 {
-	// TODO remove opengl learning
-	float positions[] =
-	{
-		-0.5f, -0.5f, 0.0f, 0.0f,
-		 0.5f, -0.5f, 1.0f, 0.0f,
-		 0.5f,  0.5f, 1.0f, 1.0f,
-		-0.5f,  0.5f, 0.0f, 1.0f
-	};
+    // TODO remove opengl learning
+    float positions[] =
+    {
+        -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f
+    };
 
-	m_Shader.AddShader(GL_VERTEX_SHADER, "VertexShader.hlsl");
-	m_Shader.AddShader(GL_FRAGMENT_SHADER, "PixelShader.hlsl");
-	m_Shader.CreateShader();
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
 
-	unsigned int indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
+    VertexArray(va);
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
-	m_IndexBuffer = IndexBuffer(indices, 6);
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
 
-	unsigned int vao;
-	GLCall(glGenVertexArrays(1, &vao));
-	GLCall(glBindVertexArray(vao));
+    glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
 
-	VertexArray(va);
-	VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+    m_Shader.Bind();
+    m_Shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    m_Shader.SetUniformMat4f("u_MVP", proj);
 
-	VertexBufferLayout layout;
-	layout.Push<float>(2);
-	layout.Push<float>(2);
-	va.AddBuffer(vb, layout);
+    //Texture texture("graphics/background.png");
+    int textureSlot = 0;
+    m_Texture.Bind(textureSlot);
+    m_Shader.SetUniform1i("u_Texture", textureSlot);
 
-	glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+    va.Unbind();
+    m_Shader.Unbind();
+    vb.Unbind();
+    m_IndexBuffer.Unbind();
 
-	m_Shader.Bind();
-	m_Shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-	m_Shader.SetUniformMat4f("u_MVP", proj);
-
-	//Texture texture("graphics/background.png");
-	int textureSlot = 0;
-	m_Texture.Bind(textureSlot);
-	m_Shader.SetUniform1i("u_Texture", textureSlot);
-
-	va.Unbind();
-	m_Shader.Unbind();
-	vb.Unbind();
-	m_IndexBuffer.Unbind();
-
-	renderer.Draw(va, m_IndexBuffer, m_Shader);
+    renderer.Draw(va, m_IndexBuffer, m_Shader);
 }
